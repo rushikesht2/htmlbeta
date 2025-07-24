b49c8646b52d0037638338897a029011923cdaf3
Rushikesh Tayade <rushikeshtayade201@gmail.com>
2025-07-24 19:20:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ // This will run when the page loads
+    window.onload = function() {
+      alert("Welcome to my website!");
+    };
+
+    // Function for the button
+    function showMessage() {
+      document.getElementById("output").innerText = "You clicked the button!";
+    }
